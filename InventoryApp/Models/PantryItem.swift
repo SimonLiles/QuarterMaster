7f7ad60aa63837b34b5d074474530bcaaf293837
SimonLiles <simon@sveoti.net>
2022-01-10 19:41:04 -0500

    Updated PantryItem documentation

--- InventoryApp/Models/PantryItem.swift
+++ InventoryApp/Models/PantryItem.swift
@@ -14,18 +14,16 @@ import Foundation
  The backbone of the pantry inventory management software, the PantryItem model. The PantryItem model is subclassed as Codable,  Equatable, and Hashable, thus allowing it to be encoded, decoded, equated, and be used as a key in dictionaries.
  
  - Variables: a pantryItem has a name, category, location, current quantity, needed quantity, units, a note, and a purchase status
- ~~~
- var name: String //name of the item
- var category: String //Category the item belongs to
- var location: String //Where the item is stored
- var currentQuantity: Double //How much of the item is currently being stored
- var neededQuantity: Double = 1 //How much of the item is needed, preset to 1
- var units: String //How the amount of the item is measured
- 
- var note: String //Allows user to add notes regarding the item
+    - name: String, name of the item
+    - category: String, Category the item belongs to
+    - location: String, Where the item is stored
+    - currentQuantity: Double, How much of the item is currently being stored
+    - neededQuantity: Double, How much of the item is needed, defaults to 1
+    - units: String, How the amount of the item is measured
+    - note: String, Allows user to add notes regarding the item
  
  var purchaseStatus: PurchaseStatus = .toBuy //Indicates wether an item has been purchased, canceled, or needs to be bought
- ~~~
+ 
  
  - Important: Initialization of the PantryItem object can only be done with a name, category, location, currentQuantity, units, and the note, the other values must be set outside of the initializer
  */
